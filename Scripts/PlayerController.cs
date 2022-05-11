7c7a7fa8ee1d72d44309dbbaa4163993ac5615b4
GCLuis <2604243189@qq.com>
2022-05-11 20:11:15 +0800

    增加相机跟随 优化地图 增加普通攻击

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rbd;
     private Animator anim;
+    private bool canMove;
 
     // Start is called before the first frame update
     void Start()
     {
         rbd = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        canMove = true;
     }
 
     // Update is called once per frame
@@ -20,27 +22,46 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         SwitchAnim();
+        Attack();
     }
 
     void Movement()
     {
-
-        rbd.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed);
-
+        if (canMove)
+        {
+            rbd.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed);
+        }
     }
 
     void SwitchAnim()
     {
-        if(Input.GetAxisRaw("Horizontal") != 0) //转换朝向
+        if(Input.GetAxisRaw("Horizontal") != 0 & canMove) //转换朝向
         {
             transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1,1);
         }
         
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0 | Mathf.Abs(Input.GetAxis("Vertical")) > 0)  //跑步动画切换
+        if ((Mathf.Abs(Input.GetAxis("Horizontal")) > 0 | Mathf.Abs(Input.GetAxis("Vertical")) > 0) & canMove )  //跑步动画切换
         {
             anim.SetBool("Moving", true);
         }
         else { anim.SetBool("Moving", false); }
     }
 
+    void Attack()
+    {
+        if(Input.GetButtonDown("attack"))
+        {
+            canMove = false;
+            Debug.Log(canMove);
+            anim.SetTrigger("Attack");
+            rbd.velocity = new Vector2 (0,0);
+            Invoke("resetMove", 1);
+        }
+
+    }
+
+    void resetMove()
+    {
+        canMove = true;
+    }
 }
